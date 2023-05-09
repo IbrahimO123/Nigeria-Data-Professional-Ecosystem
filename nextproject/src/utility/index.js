5abba491f70b3be506e9bb7018219a472f2330dc
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-05-09 12:58:03 +0100

    added filter for empty string

--- nextproject/src/utility/index.js
+++ nextproject/src/utility/index.js
@@ -3,12 +3,16 @@
 export const handleGetDataset = (list, key, role) => {
   const obj = {};
   const data = list.filter((item) => item.job_title === role);
-  const response = data.map((item) => item[key]).filter((res) => res !== "[]" || res !== undefined || res !== null);
-  
+  const response = data
+    .map((item) => item[key])
+    .filter((res) => res !== "[]" || res !== undefined);
+
   const word = response.map((item) =>
     item.slice(1, item.length - 1).split(",")
   );
-  const simple = word.reduce((a, b) => a.concat(b), []);
+  const simple = word
+    .reduce((a, b) => a.concat(b), [])
+    .filter((res) => res !== "");
   for (let i = 0; i < simple.length; i++) {
     let item = simple[i]
       .trim()
